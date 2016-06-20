7344c00e74045cf31f6c0ea3a404efe63d19ca95
amc333 <anniecalpito@192-168-1-6.tpgi.com.au>
2016-06-20 20:34:08 +1000

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_one :profile
+  has_many :status_updates
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :status_updates
+  resources :profiles
+  devise_for :users
+  #get 'home/index'
+  root 'home#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
